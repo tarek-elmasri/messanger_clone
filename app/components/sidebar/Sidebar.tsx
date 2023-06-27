3b6645b8ee598434a18cfa9c78ca10b30c3fb06b
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2023-06-27 03:24:55 +0300

    [feat] Adds user page layout and sidebars

---
+++ app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,18 @@
+import React, { use } from "react";
+import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+const Sidebar = ({ children }: { children: React.ReactNode }) => {
+  const currentUser = use(getCurrentUser());
+
+  return (
+    <div className="h-full">
+      <DesktopSidebar user={currentUser!} />
+      <MobileFooter />
+      <main className="lg:pl029 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ app/hooks/useConversation.ts
@@ -0,0 +1,26 @@
+import { useParams } from "next/navigation";
+import { useMemo } from "react";
+
+const useConversation = () => {
+  const params = useParams();
+
+  const conversationId = useMemo(
+    () => params?.conversationId ?? "",
+    [params?.conversationId]
+  );
+
+  const isOpen = useMemo(
+    () => !!params?.conversationId,
+    [params?.conversationId]
+  );
+
+  return useMemo(
+    () => ({
+      isOpen,
+      conversationId,
+    }),
+    [isOpen, conversationId]
+  );
+};
+
+export default useConversation;
